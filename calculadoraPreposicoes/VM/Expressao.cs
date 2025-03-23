4882ddb33a9cd3f9329a2b4a1e7a7de525708100
tiagostmg <tiago.taumaturgo@gmail.com>
2025-03-23 10:51:48 -0300

    .

--- calculadoraPreposicoes/VM/Expressao.cs
+++ calculadoraPreposicoes/VM/Expressao.cs
@@ -1,12 +1,10 @@
-﻿using calculadoraPreposicoes.Validacao;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace calculadoraPreposicoes.VM
 {
     public class Expressao
     {
         [Required(ErrorMessage = "A expressão é obrigatória.")]
-        //[ExpressaoValidacao(ErrorMessage = "A expressão não é válida.")]
         public string expressao { get; set; }
     }
 }
